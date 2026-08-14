71890b8f5dbf8ab269e7222c0894b768e23b20d3
akicatt <markpustog@gmail.com>
2026-08-14 20:39:11 +0500

    separated from parameters.hpp

--- core/parameters/parameters_accessor.cpp
+++ core/parameters/parameters_accessor.cpp
@@ -1,26 +1,43 @@
 //
-// Created by down on 13.08.2026.
+// Created by akicatt on 14.08.2026.
 //
 
 #include "parameters_accessor.hpp"
-#include "parameters.hpp"
 
-template <mydak::tools::static_string Option>
-consteval auto mydak::parameters_accessor::get_by_static_string() const {
-    using type = std::decay_t<decltype(args::get_type_sequence())>;
-    const auto N = args::options_indices.consteval_at<Option>();
+[[nodiscard]] std::array<mydak::args::parameter_variants, mydak::args::parameters_count> mydak::args::parameters_accessor::process_args( int argc, char* argv[]) {
+    auto new_parameters = parameters;
+    immortal_strings.resize(128);
 
-    return std::get<tools::at<N, type>::value>(parameters_internal[N]).get_data();
-}
+    for (int i = 1; i < argc; i++) {
+        std::string raw = argv[i];
 
-template <std::size_t N>
-consteval auto mydak::parameters_accessor::get_by_index(std::integral_constant<std::size_t, N>) {
+        if (raw == "--help") help();
 
-}
+        const auto equals_pos = raw.find('=');
+        if (equals_pos == std::string::npos) {
+            logger::exit(std::format("Wrong parameter format: {}! (no equals symbol)", raw));
+        }
 
-template <std::size_t N>
-constexpr mydak::parameters_accessor::parameters_accessor(std::array<args::parameter_variants, N> parameters_internal) {
-    for (auto& variant : parameters_internal) {
-        this->parameters_internal.emplace_back(&variant);
+        std::string parameter_string = raw.substr(0, equals_pos);
+        immortal_strings[i] = std::string(raw.subview(equals_pos + 1, raw.size() - equals_pos - 1));
+        const auto& value = immortal_strings[i];
+
+        if (value.empty()) {
+            logger::exit(std::format("Empty value: {}!", parameter_string));
+        }
+
+        auto opt = options_indices.at(parameter_string);
+
+        if (!opt.has_value()) {
+            logger::exit(std::format("Wrong parameter: {}! seek help: --help.", parameter_string));
+        }
+
+        const size_t& index = opt.value();
+        parameter_variants& variants = new_parameters[index];
+        variants.visit([&](auto&& parameter) {
+            parameter.try_set_val(value.c_str());
+        });
     }
+
+    return new_parameters;
 }
--- core/parameters/parameters_accessor.hpp
+++ core/parameters/parameters_accessor.hpp
@@ -1,41 +1,41 @@
 //
-// Created by down on 13.08.2026.
+// Created by akicatt on 14.08.2026.
 //
 
 #ifndef MYDAK_SERVER_PARAMETERS_ACCESSOR_H
 #define MYDAK_SERVER_PARAMETERS_ACCESSOR_H
-#include "tools.hpp"
-#include <vector>
 
 #include "parameters.hpp"
 
-
-namespace mydak {
+namespace mydak::args {
     struct parameters_accessor {
         constexpr ~parameters_accessor() = default;
         constexpr parameters_accessor() = default;
-        template <std::size_t N>
-        constexpr parameters_accessor(const std::array<args::parameter_variants, N>& parameters_internal) : parameters_internal(parameters_internal) {}
+        parameters_accessor(const int argc, char* argv[]) : parameters_internal(process_args(argc, argv)) {}
 
         template <std::size_t N>
         constexpr auto get() const {
             // Getting type from our magic constexpr type_indices
-            using type = std::decay_t<decltype(args::type_sequence)>;
+            using type = std::decay_t<decltype(type_sequence)>;
 
             return std::get<tools::at<N, type>::value>(parameters_internal[N]).get_data();
         }
 
         template <tools::static_string Option>
         constexpr auto get() const {
-            using type = std::decay_t<decltype(args::get_type_sequence())>;
-            const auto N = args::options_indices.consteval_at<Option>();
+            using type = std::decay_t<decltype(get_type_sequence())>;
+            const auto N = options_indices.consteval_at<Option>();
 
             return std::get<tools::at<N, type>::value>(parameters_internal[N]).get_data();
         }
     private:
-        std::vector<args::parameter_variants*> parameters_internal;
+        [[nodiscard]] static std::array<parameter_variants, parameters_count> process_args( int argc, char* argv[]);
+
+        std::array<parameter_variants, parameters_count> parameters_internal;
     };
 }
+
+
 
 
 #endif //MYDAK_SERVER_PARAMETERS_ACCESSOR_H
