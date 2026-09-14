c0ed10e0c48a6e2f7a4218b5f30055aefa3b61c0
akicatt <markpustog@gmail.com>
2026-09-15 00:22:14 +0500

    codes

--- core/util/codes.hpp
+++ core/util/codes.hpp
@@ -6,12 +6,25 @@
 #define MYDAK_SERVER_CODES_H
 #include <cstddef>
 
-namespace mydak::codes {
-    constexpr std::uint8_t NO_CLIENT = 0;
-    constexpr std::uint8_t EXPIRED_CLIENT = 1;
-    constexpr std::uint8_t BAD_SIGNAL = 2;
-    constexpr std::uint8_t SUCCESS = 3;
-    constexpr std::uint8_t EXCEPTION = -1;
+namespace mydak {
+    enum class send_codes {
+        NO_CLIENT,
+        EXPIRED_CLIENT,
+        BAD_SIGNAL,
+        SUCCESS,
+        EXCEPTION
+    };
+    namespace send_strings {
+        constexpr std::string_view NO_CLIENT =
+            "No client with that index";
+        constexpr std::string_view EXPIRED_CACHED_CLIENT =
+            "Cached client is expired, getting new one!";
+        constexpr std::string_view EXPIRED_CACHED_CLIENT_SECOND_TRY =
+            "Cached client is still somehow expired after getting new one!";
+        constexpr std::string_view BAD_SIGNAL =
+            "Idk how you managed to fuck with signal channel.";
+    }
+
 }
 
 #endif //MYDAK_SERVER_CODES_H
