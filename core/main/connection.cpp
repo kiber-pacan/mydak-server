9070229a53ebfab730307cedb85ab0511e491000
akicatt <markpustog@gmail.com>
2026-09-15 00:22:30 +0500

    refactoring

--- core/main/connection.cpp
+++ core/main/connection.cpp
@@ -18,60 +18,27 @@
 #include "logger.hpp"
 #include "codes.hpp"
 
-constexpr std::string_view FUCKED_UP_GREETNGS_SYMBOL =
+constexpr std::string_view FUCKED_UP_GREETINGS_SYMBOL =
 	"First symbol aka greetings[0] is != \"0x67\"!";
 constexpr std::string_view FUCKED_UP_MESSAGE_SIZE =
-	"Total message size is less than 1 or more than 512 symbols!";
+	"Total message size is less not in bounds!";
 constexpr std::string_view NO_CLIENT_WITH_THAT_KEY =
 	"No client with that key!";
 constexpr std::string_view CONNECTION_ENDED =
 	"Connection ended!";
-constexpr std::string_view NO_CLIENT =
-	"No client with that index";
-constexpr std::string_view EXPIRED_CACHED_CLIENT =
-	"Cached client is expired, getting new one!";
-constexpr std::string_view EXPIRED_CACHED_CLIENT_SECOND_TRY =
-	"Cached client is still somehow expired after getting new one!";
-constexpr std::string_view BAD_SIGNAL =
-	"Idk how you managed to fuck with signal channel.";
-constexpr std::string_view DEFAULT_CASE =
-	"DEFAULT_CASE (something may be horribly wrong)";
 
-std::shared_ptr<asio::ip::tcp::socket> mydak::connection::getSocket() {
-	return socket;
-}
-
-mydak::client_index mydak::connection::get_recipient_index(const std::array<unsigned char, proto::E2E_KEYS_RAW_L>& recipient) {
-	return server->get_client_index(recipient);
-}
 
 
 asio::awaitable<void> mydak::connection::start() {
 	// Voodoo type shi to keep coroutine alive after it's poiner death
 	auto self = shared_from_this();
 
-	//asio::socket_base::keep_alive option(true);
-	//socket->set_option(option);
-
 	signal_channel = std::make_shared<receive_signal>(socket->get_executor());
 
 	try {
-
-		// Another fucking bullshit.
-		//
-		//asio::awaitable<size_t> bytes_awaitable = asio::async_read(socket, asio::buffer(rawkey), asio::use_awaitable);
-		//size_t bytes = co_await static_cast<asio::awaitable<size_t>&&>(bytes_awaitable); 
-		
-		
 		// Still bullshit, but a little bit prettier.
 		asio::awaitable<size_t> bytes_awaitable = asio::async_read(*socket.get(), asio::buffer(public_key), asio::use_awaitable);
 		co_await std::move(bytes_awaitable);
-			
-
-		// Look at that beauty! Unfortunately asio::awaitable<> dies because it's an rvalue. 
-		//
-		// size_t bytes = co_await asio::async_read(socket, asio::buffer(key), asio::use_awaitable);
-
 
 		asio::ip::address ip = socket->remote_endpoint().address();
 			
@@ -93,28 +60,24 @@ asio::awaitable<void> mydak::connection::start() {
 			std::array<char, proto::GREETINGS_PREFIX_L + proto::MESSAGE_SIZE_L + proto::E2E_KEYS_RAW_L> greetings{};
 			co_await asio::async_read(*socket.get(), asio::buffer(greetings, greetings.size()), asio::use_awaitable);
 
-
 			// Check if prefix is right
 			if (greetings[0] != proto::GREETINGS_PREFIX) {
-				logger::log_debug_error(FUCKED_UP_GREETNGS_SYMBOL);
+				logger::log_debug_error(FUCKED_UP_GREETINGS_SYMBOL);
 				break;
 			}
 
 			// Copying message size from greetings
 			uint32_t message_size;
-			std::memcpy(&message_size, std::span(greetings).subspan(1,4).data(), 4);
+			std::memcpy(&message_size, greetings.data() + 1, 4);
 
 			// We get message in little endian
 			if constexpr (std::endian::native == std::endian::big) message_size = std::byteswap(message_size);
 
-			std::uint32_t min = 0;
-			std::uint32_t max = 512;
 			// Checking if message_size is in boundaries
-			// TODO FIX
-			/*if (message_size > min && message_size < max) {
+			if (message_size < proto::MIN_MESSAGE_SIZE || message_size > proto::MAX_MESSAGE_SIZE) {
 				logger::log_debug_error(std::format("{} ({})", FUCKED_UP_MESSAGE_SIZE, message_size));
 				break;
-			}*/
+			}
 
 			// Copying recipient into array from greetings
 			constexpr std::size_t message_start = proto::GREETINGS_PREFIX_L + proto::MESSAGE_SIZE_L;
@@ -126,7 +89,6 @@ asio::awaitable<void> mydak::connection::start() {
 			);
 
 			// Receiving message
-
 			std::vector<char> message{};
 			message.resize(message_size);
 			co_await asio::async_read(*socket.get(), asio::buffer(message, message_size), asio::use_awaitable);
@@ -154,11 +116,11 @@ asio::awaitable<void> mydak::connection::start() {
 
 			size_t tries = 0;
 
+			#pragma region Sending
 			// Evil goto
 		    add_message_to_queue:
 
-			const client_index recipient_index = get_recipient_index(recipient);
-
+			const client_index recipient_index = server->get_client_index(recipient);
 
 			// If no client with that public key is currently online
 			// we add the queued message to the mariadb database
@@ -167,44 +129,37 @@ asio::awaitable<void> mydak::connection::start() {
 				continue;
 			}
 
-
 			// Trying to add queued message to the queue and processing the code
-			const uint8_t code =
+			const auto code =
 				co_await server->add_message_to_queue(recipient_index.index, recipient_index.generation, queued_message);
 
 			switch (code) {
 				// No client with that index
-			    case codes::NO_CLIENT: {
-					logger::log_debug_error(NO_CLIENT);
+			    case send_codes::NO_CLIENT: {
+					logger::log_debug_error(send_strings::NO_CLIENT);
 					break;
 				}
 				// Wrong  generation
-			    case codes::EXPIRED_CLIENT: {
-					logger::log_debug_error(EXPIRED_CACHED_CLIENT);
+			    case send_codes::EXPIRED_CLIENT: {
+					logger::log_debug_error(send_strings::EXPIRED_CACHED_CLIENT);
 
 					// If we somehow got another expired client
-					if (tries++ >= 1) {
-						logger::log_debug_error(EXPIRED_CACHED_CLIENT_SECOND_TRY);
+					if (tries++ >= 2) {
+						logger::log_debug_error(send_strings::EXPIRED_CACHED_CLIENT_SECOND_TRY);
 						break;
 					}
 					goto add_message_to_queue; // Evil goto hack to try again without expired cached message
 				}
 				// Signal failure
-			    case codes::BAD_SIGNAL: {
-					logger::log_debug_error(BAD_SIGNAL);
+			    case send_codes::BAD_SIGNAL: {
+					logger::log_debug_error(send_strings::BAD_SIGNAL);
 					break;
 				}
 				// Success
-			    case codes::SUCCESS: {
-			    	logger::log_func_debug("SUCCESS");
-					break;
-			    }
-				case codes::EXCEPTION: {
-					logger::log_func_debug_error("FUCK");
-					break;
-				}
-				default: logger::log_func_debug_error(DEFAULT_CASE);
+			    case send_codes::SUCCESS: {}
+				case send_codes::EXCEPTION: {}
 			}
+			#pragma endregion
 		}
 	}
 	catch (const boost::system::system_error& e) {
